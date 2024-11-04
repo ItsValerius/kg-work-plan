fea34380cf5165e8b40e345117eac87b5d61896a
valentin kueck <kueck.valentin@googlemail.com>
2024-11-04 18:30:22 +0100

    add compliancy pages

--- src/app/events/layout.tsx
+++ src/app/events/layout.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import Footer from "@/components/Footer";
 import SignInButton from "@/components/SignInButton";
 
 export default async function EventsLayout({
@@ -9,9 +10,10 @@ export default async function EventsLayout({
   const user = (await auth())?.user;
 
   return (
-    <div className="px-2">
+    <div className="px-2 flex flex-col justify-between min-h-screen">
       {!user && <SignInButton />}
       {children}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Button asChild variant="link">
+        <Link href="/datenschutz">Datenschutz</Link>
+      </Button>
+      <Button asChild variant="link">
+        <Link href="/impressum">Impressum</Link>
+      </Button>{" "}
+      <Button asChild variant="link">
+        <Link href="/nutzungsbedingungen">Nutzungsbedingungen</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default Footer;
