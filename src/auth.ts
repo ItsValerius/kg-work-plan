fb12f2806d7702b64d579d899cea3fefdf4ce45a
valentin kueck <kueck.valentin@googlemail.com>
2024-11-12 10:12:58 +0100

    remove code

--- src/auth.ts
+++ src/auth.ts
@@ -40,7 +40,7 @@ const providers: Provider[] = [
       },
     },
     from: process.env.EMAIL_FROM,
-    sendVerificationRequest,
+    // sendVerificationRequest,
   }),
 
   Google,
@@ -85,80 +85,80 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
 });
 
-export async function sendVerificationRequest(params: {
-  identifier: string;
-  url: string;
-  provider: any;
-  theme: any;
-}) {
-  const { identifier, url, provider, theme } = params;
-  const { host } = new URL(url);
-  console.log(identifier);
+// export async function sendVerificationRequest(params: {
+//   identifier: string;
+//   url: string;
+//   provider: any;
+//   theme: any;
+// }) {
+//   const { identifier, url, provider, theme } = params;
+//   const { host } = new URL(url);
+//   console.log(identifier);
 
-  // NOTE: You are not required to use `nodemailer`, use whatever you want.
-  const transport = createTransport(provider.server);
-  const result = await transport.sendMail({
-    to: identifier,
-    from: provider.from,
-    subject: `Sign in to ${host}`,
-    text: text({ url, host }),
-    html: html({ url, host, theme }),
-  });
-  const failed = result.rejected;
-  if (failed.length) {
-    throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
-  }
-}
+//   // NOTE: You are not required to use `nodemailer`, use whatever you want.
+//   const transport = createTransport(provider.server);
+//   const result = await transport.sendMail({
+//     to: identifier,
+//     from: provider.from,
+//     subject: `Sign in to ${host}`,
+//     text: text({ url, host }),
+//     html: html({ url, host, theme }),
+//   });
+//   const failed = result.rejected;
+//   if (failed.length) {
+//     throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
+//   }
+// }
 
-function html(params: { url: string; host: string; theme: any }) {
-  const { url, host, theme } = params;
+// function html(params: { url: string; host: string; theme: any }) {
+//   const { url, host, theme } = params;
 
-  const escapedHost = host.replace(/\./g, "&#8203;.");
+//   const escapedHost = host.replace(/\./g, "&#8203;.");
 
-  const brandColor = theme.brandColor || "#346df1";
-  const color = {
-    background: "#f9f9f9",
-    text: "#444",
-    mainBackground: "#fff",
-    buttonBackground: brandColor,
-    buttonBorder: brandColor,
-    buttonText: theme.buttonText || "#fff",
-  };
+//   const brandColor = theme.brandColor || "#346df1";
+//   const color = {
+//     background: "#f9f9f9",
+//     text: "#444",
+//     mainBackground: "#fff",
+//     buttonBackground: brandColor,
+//     buttonBorder: brandColor,
+//     buttonText: theme.buttonText || "#fff",
+//   };
 
-  return `
-<body style="background: ${color.background};">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        Sign in to <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`;
-}
+//   return `
+// <body style="background: ${color.background};">
+//   <table width="100%" border="0" cellspacing="20" cellpadding="0"
+//     style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+//     <tr>
+//       <td align="center"
+//         style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+//         Sign in to <strong>${escapedHost}</strong>
+//       </td>
+//     </tr>
+//     <tr>
+//       <td align="center" style="padding: 20px 0;">
+//         <table border="0" cellspacing="0" cellpadding="0">
+//           <tr>
+//             <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
+//                 target="_blank"
+//                 style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
+//                 in</a></td>
+//           </tr>
+//         </table>
+//       </td>
+//     </tr>
+//     <tr>
+//       <td align="center"
+//         style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+//         If you did not request this email you can safely ignore it.
+//       </td>
+//     </tr>
+//   </table>
+// </body>
+// `;
+// }
 
-// Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
-function text({ url, host }: { url: string; host: string }) {
-  return `Sign in to ${host}\n${url}\n\n`;
-}
+// // Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
+// function text({ url, host }: { url: string; host: string }) {
+//   return `Sign in to ${host}\n${url}\n\n`;
+// }
