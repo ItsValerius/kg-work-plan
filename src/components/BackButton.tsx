93314825f8658d1c3ff3bf6ca606268620584f5d
valentin kueck <kueck.valentin@googlemail.com>
2024-11-12 14:20:55 +0100

    back btn for login page

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { ArrowLeft } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export default function BackButton({ className }: { className: string }) {
+  const router = useRouter();
+  const handleBackNavigation = () => {
+    if (
+      document.referrer &&
+      document.referrer.includes(window.location.origin)
+    ) {
+      // If the referrer is from the same site, go back
+      router.back();
+    } else {
+      // Otherwise, redirect to a default route, like home
+      router.push("/events");
+    }
+  };
+  return (
+    <Button
+      variant={"outline"}
+      onClick={handleBackNavigation}
+      className={cn(className, "")}
+    >
+      <ArrowLeft />
+      Zurück
+    </Button>
+  );
+}
